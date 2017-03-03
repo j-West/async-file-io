f123f21ccd76d22071e0c5dc53e62a853adec4f5
j-west <westja89@gmail.com>
2017-03-03 10:35:43 -0600

    Add file to read and log to terminal

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+
+const { readFile } = require('fs');
+
+const fileArg = process.argv[2];
+
+
+if (fileArg) {
+  readFile(fileArg, (err, data) => {
+    if (err) return console.error('Error', err);
+    process.stdout.write(data);
+  })
+
+} else process.exit();
+
+console.log('This is the asynchronous version');
